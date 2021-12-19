0eeac1090b45aa8c98eaa7feb66a5dc4a9d93ad3
vv1ck <75469718+vv1ck@users.noreply.github.com>
2021-12-19 14:09:15 -0500

    Create API_search.py

---
+++ API_search.py
@@ -0,0 +1,11 @@
+#API_search_for_a_user:
+" requests = get "
+url = 'https://api.c2me.cc/b/search?checksum=5029862947158448&nick= { enter your username } &o=6agufF52tU5E8u0dnROjZPl25a4tLFhX&password= {enter your password } &s= {enter target}'
+headers = {
+    'Host': 'api.c2me.cc',
+    'Accept': '*/*',
+    'Accept-Language': 'ar-JO;q=1, en-JO;q=0.9',
+    'Connection': 'close',
+    'Accept-Encoding': 'gzip, deflate',
+    'User-Agent': 'Connected2/1.301.2112140025 (iPhone; iOS 13.5; Scale/2.00)',
+}
