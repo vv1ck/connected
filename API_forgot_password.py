17115694a1dbc7f6a80346267b58cb65e1ba53d6
vv1ck <75469718+vv1ck@users.noreply.github.com>
2021-12-19 14:11:07 -0500

    Create API_forgot_password.py

---
+++ API_forgot_password.py
@@ -0,0 +1,14 @@
+#API_forgot_password:
+" requests = post "
+url = "https://api.c2me.cc/b/forgot_password"
+headers = {
+'Host': 'api.c2me.cc',
+'Content-Type': 'application/x-www-form-urlencoded',
+'Connection': 'close',
+'Accept': '*/*',
+'User-Agent': 'Connected2/1.301.2112140025 (iPhone; iOS 13.5; Scale/2.00)',
+'Accept-Language': 'ar-JO;q=1, en-JO;q=0.9',
+'Accept-Encoding': 'gzip, deflate',
+'Content-Length': '87',
+}
+data = "checksum=547996441674972655&nick= { enter email/username } &o=7dRwvdvd30saSY4zQvEPE8cI1tCvHMSw"
