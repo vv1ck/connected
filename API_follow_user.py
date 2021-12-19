f600852d478c360b08894f0ca5d0689633df6f83
vv1ck <75469718+vv1ck@users.noreply.github.com>
2021-12-19 14:08:34 -0500

    Create API_follow_user.py

---
+++ API_follow_user.py
@@ -0,0 +1,11 @@
+#API_follow_user:
+" requests = get "
+url = 'https://api.c2me.cc/b/follow?checksum=424297204859392613&follow_nick= { enter target } &nick= { enter your username }&o=c0VloEUhcLeQJrkCiLMQdpTAjA3Ky4EQ&password= { enter your password}'
+headers = {
+    'Host': 'api.c2me.cc',
+    'Accept': '*/*',
+    'Accept-Language': 'ar-JO;q=1, en-JO;q=0.9',
+    'Connection': 'close',
+    'Accept-Encoding': 'gzip, deflate',
+    'User-Agent': 'Connected2/1.301.2112140025 (iPhone; iOS 13.5; Scale/2.00)',
+}
