a1d6a00a7ef2031ebbd756747af2d82b4181349a
vv1ck <75469718+vv1ck@users.noreply.github.com>
2021-12-19 14:10:35 -0500

    Create API_change_username.py

---
+++ API_change_username.py
@@ -0,0 +1,14 @@
+#API_change_username:
+" requests = post "
+url = 'https://api.c2me.cc/b/nick_change'
+headers={
+'Host': 'api.c2me.cc',
+'Content-Type': 'application/x-www-form-urlencoded',
+'Connection': 'close',
+'Accept': '*/*',
+'User-Agent': 'Connected2/1.301.2112140025 (iPhone; iOS 13.5; Scale/2.00)',
+'Accept-Language': 'ar-JO;q=1, en-JO;q=0.9',
+'Accept-Encoding': 'gzip, deflate',
+'Content-Length': '110',
+}
+data = "checksum=923175201987719563&new_nick= { new username } &nick= { old username } &o=AkzuZL3HyBqWgSbAq6LtkRtutfcviGyd&password= { your password"
